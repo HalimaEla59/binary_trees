8ffcf69f372f3be435264e97ee48b61d7c3bdc77
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-11-01 23:58:50 +0100

    14-binary_tree_balance.c added

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: balance, tree is NULL 0.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right, balance;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	balance = left - right;
+	return (balance);
+}
