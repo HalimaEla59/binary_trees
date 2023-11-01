1836488d8f46ca5acf392335522aba6bef2bd6ef
Halima EL-ABBADI <86242444+HalimaEla59@users.noreply.github.com>
2023-11-01 23:35:10 +0100

    11-binary_tree_size.c added

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to root node of the tree to measure
+ * Return: size of tree, If tree is NULL 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+
+	if (!(tree))
+		return (0);
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+	return (lsize + rsize + 1);
+}
